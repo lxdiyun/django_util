aee40f8bc171b18ab39296b1d94a2c33ea262227
lxdiyun <lxdiyun@gmail.com>
2013-08-03 12:35:20 +0800

    add whoosh backends

--- haystack/forms.py
+++ haystack/forms.py
@@ -9,26 +9,26 @@ class MultiSearchForm(ModelSearchForm):
         cleaned_data = super(MultiSearchForm, self).clean()
         q = cleaned_data.get('q')
         q = jieba_segment(q)
-        cleaned_data['q'] = q
+#        cleaned_data['q'] = q
 
         return cleaned_data
 
-    def search(self):
-        if not self.is_valid():
-            return self.no_query_found()
-
-        if not self.cleaned_data.get('q'):
-            return self.no_query_found()
-
-        terms = self.cleaned_data['q'].split(" ")
-        sq = SQ()
-
-        for term in terms:
-            sq.add(SQ(content=AutoQuery(term)), SQ.AND)
-
-        sqs = self.searchqueryset.filter(sq)
-
-        if self.load_all:
-            sqs = sqs.load_all()
-
-        return sqs
+#    def search(self):
+#        if not self.is_valid():
+#            return self.no_query_found()
+#
+#        if not self.cleaned_data.get('q'):
+#            return self.no_query_found()
+#
+#        terms = self.cleaned_data['q'].split(" ")
+#        sq = SQ()
+#
+#        for term in terms:
+#            sq.add(SQ(content=AutoQuery(term)), SQ.AND)
+#
+#        sqs = self.searchqueryset.filter(sq)
+#
+#        if self.load_all:
+#            sqs = sqs.load_all()
+#
+#        return sqs
