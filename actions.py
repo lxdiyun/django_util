8aa465f207f3a7659faa48e2ae5511c27c3a19df
Adrain Lee <lxdiyun@gmail.com>
2013-10-18 17:07:52 +0800

    add export action(not admin)

---
+++ actions.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from admin_actions import prep_field
+from utils import UnicodeWriter
+
+
+def export_as_csv(filename, field_names, queryset, header=None):
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % (
+            unicode(filename).replace('.', '_')
+        )
+
+        writer = UnicodeWriter(response)
+
+        if header:
+            writer.writerow(header)
+
+        for obj in queryset:
+            writer.writerow([prep_field(obj, field) for field in field_names])
+
+        return response
--- admin_actions.py
+++ admin_actions.py
@@ -1,4 +1,4 @@
-from zenshu.utils import UnicodeWriter, get_merged_objects
+from utils import UnicodeWriter, get_merged_objects
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import PermissionDenied
 from django.db import router
