5fc8e5e98f9e67b07cf647b6fd3e8a099d1c6c33
lxdiyun <lxdiyun@gmail.com>
2014-02-02 21:04:38 +0800

    add gmap Opacitycontrol js libraray

--- templates/utils/prettyPhoto/init.js
+++ templates/utils/prettyPhoto/init.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: null,
-		deeplinking: false });
+		deeplinking: false,
+		overlay_gallery: true,
+		overlay_gallery_max: 5
+	});
 });
